e635ca8227b6ad91859bc68612b0b4e342536d30
Schwonky <jonathanbillings64@gmail.com>
2026-07-14 22:23:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,47 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
-# import os and use it to get the Github repository secrets
+load_dotenv()
+
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
 MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+api_key = os.environ.get("OWM_API_KEY")
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = "43d04b46e1fe629ae1e2c86bdc963516"
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+weather_params = {
+    "lat": 45.549900,
+    "lon": -75.782593
+    "appid": api_key,
+    "cnt": 4,
+}
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+will_rain = False
+for day in weather_data["list"]:
+    weather_id = day["weather"][0]["id"]
+    if int(weather_id) < 700:
+        will_rain = True
+
+body = "The weather today will require you to bring an umbrella!" if will_rain \
+    else "The weather today will not require you to bring an umbrella!"
+
+message = Mail(
+    from_email=MY_EMAIL,
+    to_emails="schwankynator@gmail.com",
+    subject="Hello",
+    plain_text_content=body,
+)
+
+sg = SendGridAPIClient(SENDGRID_API_KEY)
+try:
+    sg_response = sg.send(message)
+    print(sg_response.status_code)
+except Exception as e:
+    print(e.body)
